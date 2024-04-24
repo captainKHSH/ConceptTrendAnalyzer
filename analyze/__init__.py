ea3ca874673949753a469154c0fa51b96448bce0
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 00:52:37 -0400

    Update __init__.py

--- analyze/__init__.py
+++ analyze/__init__.py
@@ -1,4 +1,4 @@
 """Initialization file for the Concept Trend Analyzer package."""
 # Initialization code for the CTA.analyze subpackage
 # Import modules from the api subpackage
-from . import concept_search, concept_trend_analyzer, RelatedConcepts
+from . import concept_search, concept_trend_analyzer, Related_Concepts
