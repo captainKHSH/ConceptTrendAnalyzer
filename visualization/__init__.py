1e8bb9c59ad534eee7dfda7c5f11ccf8292dfc87
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 00:55:40 -0400

    Create __init__.py

---
+++ visualization/__init__.py
@@ -0,0 +1,4 @@
+"""Initialization file for the Concept Trend Analyzer package."""
+# Initialization code for the CTA.visualization subpackage
+# Import modules from the visualization subpackage
+from . import generate_charts
