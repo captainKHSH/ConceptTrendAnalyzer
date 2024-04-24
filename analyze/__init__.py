755fac0ebbfec0a561df88ec641736138db78813
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 15:06:34 -0400

    Update __init__.py

--- analyze/__init__.py
+++ analyze/__init__.py
@@ -1,5 +1,5 @@
 """Module for analyzing concepts and trends."""
 
 from concept_search import ConceptSearch
-from concept_trend_analyzer import ConceptTrendAnalyzer
 from RelatedConcepts import RelatedConcepts
+from concept_trend_analyzer import ConceptTrendAnalyzer
