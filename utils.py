41c6a7e194b598b33641b57d1ce9f973ac5ba5b1
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 15:01:23 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 """Utility functions for the Concept Trend Analyzer package."""
 
 # Import necessary classes from their respective modules
-from .analyze.concept_search import ConceptSearch
-from .analyze.RelatedConcepts import RelatedConcepts
-from .analyze.concept_trend_analyzer import ConceptTrendAnalyzer
-from .visualization.generate_charts import GenChart
+from analyze.concept_search import ConceptSearch
+from analyze.RelatedConcepts import RelatedConcepts
+from analyze.concept_trend_analyzer import ConceptTrendAnalyzer
+from visualization.generate_charts import GenChart
 
 print("Importing utilities for the Concept Trend Analyzer package...")
 
