9aaa824565635f638f6ddfce04a35f906f1d4791
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 12:41:17 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""Initialization file for the Concept Trend Analyzer package."""
+
+# Print example usage code
+print("# Usage code \n ")
+
+print("To Search Concept")
+print("CS.search('CONCEPT')")
+print("To get related concepts")
+print("RC.related('concept_id')")
+print("To find trend over years")
+print("CAT.year('concept_id',Number of years)")
+print("To Visualization")
+print("chart.chart('concept_id',Number of years)")
