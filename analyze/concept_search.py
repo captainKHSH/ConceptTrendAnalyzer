37656c377cc5b4ac1db550daf049195ee17f0bd4
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 03:35:22 -0400

    Update concept_search.py

--- analyze/concept_search.py
+++ analyze/concept_search.py
@@ -56,6 +56,6 @@ class ConceptSearch:
         for result in results["results"]:
             concept_id = result["id"]
             display_name = result["display_name"]
-            wikipedia_link = result["wikipedia"]
+            wikipedia_link = result["wikipedia","N/A"]
             print("{:<15} {:<30} {:<50}".format(concept_id, display_name, wikipedia_link))
 
