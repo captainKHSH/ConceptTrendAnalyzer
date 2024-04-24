12483f3006f21687ed2feeff5a7280aef6d9b518
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-24 12:38:09 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 """Utility functions for the Concept Trend Analyzer package."""
 
-def example_function():
-    """An example utility function."""
-    pass
+# Import necessary classes from their respective modules
+from CTA.analyze.concept_search import ConceptSearch
+from CTA.analyze.RelatedConcepts import RelatedConcepts
+from CTA.analyze.concept_trend_analyzer import ConceptTrendAnalyzer
+from CTA.visualization.generate_charts import GenChart
+
+print("Importing utilities for the Concept Trend Analyzer package...")
+
+# Create instances of classes and store them in a dictionary
+CS = ConceptSearch()
+RC = RelatedConcepts()
+CAT = ConceptTrendAnalyzer()
+chart = GenChart()
