e73d16d6ab150b2f9d5ef28061beecd402a76906
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-26 18:41:20 -0400

    Update concept_search.py

--- analyze/concept_search.py
+++ analyze/concept_search.py
@@ -2,6 +2,7 @@
 
 
 import requests
+from tabulate import tabulate
 
 
 class ConceptSearch:
@@ -61,24 +62,9 @@ class ConceptSearch:
                 }
                 for concept in concepts_data["results"][:5]
             ]
-        # Initialize an empty string to store the formatted table
-        formatted_table = ""
+        table = []
+    for item in data:
+        table.append([item['id'], item['display_name'], item['wikipedia']])
 
-        # Add the table headers
-        formatted_table += "ID\t\t\tDisplay Name\t\t\tWikipedia Link\n"
-        formatted_table += "-" * 70 + "\n"
-
-        # Add each row of the table
-        for item in parsed_results:
-            id_str = item['id']
-            display_name_str = item['display_name']
-            wikipedia_link_str = item.get('wikipedia', '')
-
-            # Adjust tabs for better alignment
-            tabs_id = '\t' * (3 - len(id_str) // 8)
-            tabs_display_name = '\t' * (3 - len(display_name_str) // 8)
-
-            # Add the formatted row to the table
-            formatted_table += f"{id_str}{tabs_id}{display_name_str}{tabs_display_name}{wikipedia_link_str}\n"
-
-        return formatted_table
+    p = print(tabulate(table, headers=['ID', 'Display Name', 'Wikipedia Link'], tablefmt='grid'))
+    return p
