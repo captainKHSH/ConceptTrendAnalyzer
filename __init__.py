f832ec915a5c6e649b48b1b5c1b258a8fd621b1d
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-26 14:51:00 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """Module for Concept Trend Analysis (CTA)."""
 
-from ConceptTrendAnalyzer.utils import CS, RC, CAT, chart
+from utils import CS, RC, CAT, chart
 
 print("Concept Trend Analyzer package initialized.")
