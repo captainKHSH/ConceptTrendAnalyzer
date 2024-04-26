4303a77c937078713be3cda3ab7635ea52feafb9
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-26 14:59:34 -0400

    Update generate_charts.py

--- visualization/generate_charts.py
+++ visualization/generate_charts.py
@@ -1,8 +1,8 @@
 """Module for generating line charts to visualize trends."""
 
 import matplotlib.pyplot as plt
-from ConceptTrendAnalyzer.analyze.concept_trend_analyzer import ConceptTrendAnalyzer
-from ConceptTrendAnalyzer.visualization.name import Conceptname
+from analyze.concept_trend_analyzer import ConceptTrendAnalyzer
+from visualization.name import Conceptname
 
 
 class GenChart:
