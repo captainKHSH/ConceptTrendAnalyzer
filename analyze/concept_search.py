167a91c820883e3a1cc2a241d4e7cfb1d65444da
captainKHSH <74871590+captainKHSH@users.noreply.github.com>
2024-04-26 16:58:39 -0400

    Update concept_search.py

--- analyze/concept_search.py
+++ analyze/concept_search.py
@@ -78,12 +78,11 @@ class ConceptSearch:
             results (dict): A dictionary containing the search results.
         """
         formatted_results = ""
-        ini = "{:<15} {:<30} {:<50}"
 
         # Iterate over results and print each row
         for result in results["results"]:
             concept_id = result["id"]
             display_name = result["display_name"]
             wikipedia_link = result.get("wikipedia", "N/A")
-            formatted_results += ini.format(concept_id, display_name, wikipedia_link)
+            formatted_results += "{} | {} | {}\n".format(concept_id, display_name, wikipedia_link)
         return formatted_results
